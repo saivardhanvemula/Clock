94c42e30477428ee1a86fdc9f03d1d7fe6277cb4
saivardhanvemula <saivardhanvemulamncl@gmail.com>
2024-07-30 02:05:13 +0530

    minimal clock

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 let p = document.querySelector("p");
-let hrhand=document.querySelector(".hrhand")
-let minhand=document.querySelector(".minhand")
-let sechand=document.querySelector(".sechand")
-let i=0
-setInterval(() => {
+let hrhand = document.querySelector(".hrhand");
+let minhand = document.querySelector(".minhand");
+let sechand = document.querySelector(".sechand");
+let time=document.querySelector(".timecontainer")
+const setTime=()=>{
     const now = new Date();
     const hr = now.getHours();
     const min = now.getMinutes();
     const sec = now.getSeconds();
-    hrhand.style.transform=`rotate(${i}deg)`
-    i=i+1
-    console.log(hr,min,sec)
-}, 1000);
+    // console.log(min)
+    sechand.style.transform = `rotate(${sec*6-90}deg)`;
+    minhand.style.transform = `rotate(${min*6-90}deg)`;
+    hrhand.style.transform = `rotate(${hr*30-90}deg)`;
+    time.innerHTML=`${hr}:${min}:${sec} ${hr<=12 ?"AM": "PM"}`
+    // console.log(hr, min, sec);
+}
+// setTime()
+setInterval(setTime);
